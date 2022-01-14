ca3bf581ca3c1d0d84bbcc73e3979cb1a854f85c
romsaejao <76035596+romsaejao@users.noreply.github.com>
2022-01-14 20:06:49 +0700

    Update lab16_1.cpp

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -1,9 +1,24 @@
-include <iostream>
+#include <iostream>
 using namespace std;
-
+void func(int &);
 int main(){
-
-	
-	
+    int a = 5;
+    string b = "A";
+    string &c = b;
+    int *x = &a;
+    string *y = &b;
+    void *z = x;
+    cout<<a<<" "<<b<<" "<<c<<" "<<x<<" "<<y<<" "<<z<<"\n";
+    cout<<&a<<" "<<&b<<" "<<&c<<" "<<&x<<" "<<&y<<" "<<&z<<"\n";
+    c = "F";
+    b = c;
+    cout<<a<<" "<<b<<" "<<c<<" "<<x<<" "<<y<<" "<<z<<"\n";
+    *y = "W";
+    b = *y;
+    cout<<a<<" "<<b<<" "<<c<<" "<<x<<" "<<y<<" "<<z<<"\n";
+    a = 6;
+    cout<<a<<" "<<b<<" "<<c<<" "<<x<<" "<<y<<" "<<z<<"\n";
+	a = 7;
+	cout<<a<<" "<<b<<" "<<c<<" "<<x<<" "<<y<<" "<<z<<"\n";
 	return 0;
 }
